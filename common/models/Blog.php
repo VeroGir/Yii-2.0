990fa7a5b1b349553adb148bcfddb1637a91ac44
PavelMr42 <108888463+PavelMr42@users.noreply.github.com>
2024-01-15 18:03:26 +0700

    blog feature less-1

---
+++ common/models/Blog.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "blog".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $text
+ * @property string $url
+ * @property int $status_id
+ * @property int $sort
+ */
+class Blog extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'blog';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'url'], 'required'],
+            [['text'], 'string'],
+            [['status_id', 'sort'], 'integer'],
+            [['sort'], 'integer', 'max' => 99, 'min' => 1],
+            [['title', 'url'], 'string', 'max' => 150],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'Идентификатор',
+            'title' => 'Заголовок',
+            'text' => 'Текст',
+            'url' => 'ЧПУ',
+            'status_id' => 'Статус идентификатора',
+            'sort' => 'Сортировка',
+        ];
+    }
+}
